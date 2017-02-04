7bcfc889e849bd095ae440df436b6843cc978f2f
Mokry <Mokry@MOKRY-PC>
2017-02-04 16:17:13 +0100

    blank page added fix logout student layout mostly done

---
+++ DiplomaThesisSystemMVC/Controllers/EmptyPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiplomaThesisSystemMVC.Controllers
+{
+    public class EmptyPageController : Controller
+    {
+        // GET: EmptyPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DiplomaThesisSystemMVC/Models/CustomViewEngine.cs
+++ DiplomaThesisSystemMVC/Models/CustomViewEngine.cs
@@ -13,25 +13,22 @@ namespace DiplomaThesisSystemMVC.Models
             ViewLocationFormats = new[]
             {
             "~/Views/{1}/{0}.cshtml", "~/Views/{1}/{0}.vbhtml",
-            "~/Views/Common/{1}/{0}.cshtml", "~/Views/Common/{1}/{0}.vbhtml",
             "~/Views/Tabs/{1}/{0}.cshtml", "~/Views/Tabs/{1}/{0}.vbhtml",
-            "~/Views/Main_Views/{1}/{0}.cshtml", "~/Views/Main_Views/{1}/{0}.vbhtml",
+            "~/Views/Shared/{1}/{0}.cshtml", "~/Views/Shared/{1}/{0}.vbhtml",
             "~/Views/Shared/{0}.cshtml", "~/Views/Shared/{0}.vbhtml"
             };
             MasterLocationFormats = new[]
             {
             "~/Views/{1}/{0}.cshtml", "~/Views/{1}/{0}.vbhtml",
-            "~/Views/Common/{1}/{0}.cshtml", "~/Views/Common/{1}/{0}.vbhtml",
             "~/Views/Tabs/{1}/{0}.cshtml", "~/Views/Tabs/{1}/{0}.vbhtml",
-            "~/Views/Main_Views/{1}/{0}.cshtml", "~/Views/Main_Views/{1}/{0}.vbhtml",
+            "~/Views/Shared/{1}/{0}.cshtml", "~/Views/Shared/{1}/{0}.vbhtml",
             "~/Views/Shared/{0}.cshtml", "~/Views/Shared/{0}.vbhtml"
             };
             PartialViewLocationFormats = new[]
             {
             "~/Views/{1}/{0}.cshtml", "~/Views/{1}/{0}.vbhtml",
-            "~/Views/Common/{1}/{0}.cshtml", "~/Views/Common/{1}/{0}.vbhtml",
             "~/Views/Tabs/{1}/{0}.cshtml", "~/Views/Tabs/{1}/{0}.vbhtml",
-            "~/Views/Main_Views/{1}/{0}.cshtml", "~/Views/Main_Views/{1}/{0}.vbhtml",
+            "~/Views/Shared/{1}/{0}.cshtml", "~/Views/Shared/{1}/{0}.vbhtml",
             "~/Views/Shared/{0}.cshtml", "~/Views/Shared/{0}.vbhtml"
             };
         }
