9961bcac5cc4c1dbb8a93082b799b94c9aace9fa
Mokry <Mokry@MOKRY-PC>
2017-02-05 22:16:32 +0100

    fixed submit topic

--- DiplomaThesisSystemMVC/Controllers/Diploma_Thesis_Management/SubmitTopicController.cs
+++ DiplomaThesisSystemMVC/Controllers/Diploma_Thesis_Management/SubmitTopicController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DiplomaThesisSystemMVC.Models;
+using Microsoft.AspNet.Identity;
 
 namespace DiplomaThesisSystemMVC.Controllers.Diploma_Thesis_Management
 {
@@ -21,116 +22,36 @@ namespace DiplomaThesisSystemMVC.Controllers.Diploma_Thesis_Management
             return View(diplomaThesisTopic.ToList());
         }
 
-        // GET: SubmitTopic/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            DiplomaThesisTopic diplomaThesisTopic = db.DiplomaThesisTopic.Find(id);
-            if (diplomaThesisTopic == null)
-            {
-                return HttpNotFound();
-            }
-            return View(diplomaThesisTopic);
-        }
-
         // GET: SubmitTopic/Create
         public ActionResult Create()
         {
-            ViewBag.ReviewerID = new SelectList(db.Reviewer, "ID", "FirstName");
-            ViewBag.TeacherID = new SelectList(db.Teacher, "ID", "FirstName");
             return View();
         }
+
 
         // POST: SubmitTopic/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,ReviewerID,TeacherID,Name,Description,NumberOfStudents,FieldOfStudy,Degree,Availability,Status")] DiplomaThesisTopic diplomaThesisTopic)
+        public ActionResult Create([Bind(Include = "Name,Description,NumberOfStudents,FieldOfStudy,Degree")] DiplomaThesisTopic diplomaThesisTopic)
         {
+            // set default values
+            diplomaThesisTopic.TeacherID = User.Identity.GetUserId();
+            diplomaThesisTopic.Availability = "Free";
+            diplomaThesisTopic.ReviewerID = null;
+            diplomaThesisTopic.Status = "NotVoted";
+
+            //add some checks
+
             if (ModelState.IsValid)
             {
                 db.DiplomaThesisTopic.Add(diplomaThesisTopic);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            ViewBag.ReviewerID = new SelectList(db.Reviewer, "ID", "FirstName", diplomaThesisTopic.ReviewerID);
-            ViewBag.TeacherID = new SelectList(db.Teacher, "ID", "FirstName", diplomaThesisTopic.TeacherID);
             return View(diplomaThesisTopic);
         }
 
-        // GET: SubmitTopic/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            DiplomaThesisTopic diplomaThesisTopic = db.DiplomaThesisTopic.Find(id);
-            if (diplomaThesisTopic == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ReviewerID = new SelectList(db.Reviewer, "ID", "FirstName", diplomaThesisTopic.ReviewerID);
-            ViewBag.TeacherID = new SelectList(db.Teacher, "ID", "FirstName", diplomaThesisTopic.TeacherID);
-            return View(diplomaThesisTopic);
-        }
-
-        // POST: SubmitTopic/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,ReviewerID,TeacherID,Name,Description,NumberOfStudents,FieldOfStudy,Degree,Availability,Status")] DiplomaThesisTopic diplomaThesisTopic)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(diplomaThesisTopic).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.ReviewerID = new SelectList(db.Reviewer, "ID", "FirstName", diplomaThesisTopic.ReviewerID);
-            ViewBag.TeacherID = new SelectList(db.Teacher, "ID", "FirstName", diplomaThesisTopic.TeacherID);
-            return View(diplomaThesisTopic);
-        }
-
-        // GET: SubmitTopic/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            DiplomaThesisTopic diplomaThesisTopic = db.DiplomaThesisTopic.Find(id);
-            if (diplomaThesisTopic == null)
-            {
-                return HttpNotFound();
-            }
-            return View(diplomaThesisTopic);
-        }
-
-        // POST: SubmitTopic/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            DiplomaThesisTopic diplomaThesisTopic = db.DiplomaThesisTopic.Find(id);
-            db.DiplomaThesisTopic.Remove(diplomaThesisTopic);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
