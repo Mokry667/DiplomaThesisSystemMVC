4773dca822505f50a3ffa4d0e5907db79159992d
Mokry <Mokry@MOKRY-PC>
2017-02-06 10:18:42 +0100

    added browse diploma thesis topics use case added filters for browse diploma thesis topics use case

---
+++ DiplomaThesisSystemMVC/Controllers/Diploma_Thesis_Management/BrowseTopicController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DiplomaThesisSystemMVC.Models;
+
+namespace DiplomaThesisSystemMVC.Controllers.Diploma_Thesis_Management
+{
+    public class BrowseTopicController : Controller
+    {
+        private DiplomaThesisSystemDB db = new DiplomaThesisSystemDB();
+
+        // GET: BrowseTopic
+        public ActionResult Index()
+        {
+
+            var numberOfStudents = db.DiplomaThesisTopic.Select(d => d.NumberOfStudents).Distinct();
+            var fieldOfStudy = db.DiplomaThesisTopic.Select(d => d.FieldOfStudy).Distinct();
+            var degree = db.DiplomaThesisTopic.Select(d => d.Degree).Distinct();
+            var availability = db.DiplomaThesisTopic.Select(d => d.Availability).Distinct();
+
+            ViewBag.NumberOfStudents = new SelectList(numberOfStudents);
+            ViewBag.FieldOfStudy = new SelectList(fieldOfStudy);
+            ViewBag.Degree = new SelectList(degree);
+            ViewBag.Availability = new SelectList(availability);
+
+            var diplomaThesisTopic = db.DiplomaThesisTopic.Include(d => d.Reviewer).Include(d => d.Teacher);
+            return View(diplomaThesisTopic.ToList());
+        }
+
+
+        // GET: BrowseTopic/Create
+        public ActionResult Create()
+        {
+            ViewBag.ReviewerID = new SelectList(db.Reviewer, "ID", "FirstName");
+            ViewBag.TeacherID = new SelectList(db.Teacher, "ID", "FirstName");
+            return View();
+        }
+
+        // POST: BrowseTopic/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ReviewerID,TeacherID,Name,Description,NumberOfStudents,FieldOfStudy,Degree,Availability,Status")] DiplomaThesisTopic diplomaThesisTopic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DiplomaThesisTopic.Add(diplomaThesisTopic);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ReviewerID = new SelectList(db.Reviewer, "ID", "FirstName", diplomaThesisTopic.ReviewerID);
+            ViewBag.TeacherID = new SelectList(db.Teacher, "ID", "FirstName", diplomaThesisTopic.TeacherID);
+            return View(diplomaThesisTopic);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Filter([Bind(Include = "NumberOfStudents,FieldOfStudy,Degree,Availability")] DiplomaThesisTopic diplomaThesisTopic)
+        {
+
+            //var topicList = db.DiplomaThesisTopic.Include(d => d.NumberOfStudents == diplomaThesisTopic.NumberOfStudents);
+            var topicList = db.DiplomaThesisTopic.Include(d => d.Reviewer).Include(d => d.Teacher);
+
+            var numberOfStudents = db.DiplomaThesisTopic.Select(d => d.NumberOfStudents).Distinct();
+            var fieldOfStudy = db.DiplomaThesisTopic.Select(d => d.FieldOfStudy).Distinct();
+            var degree = db.DiplomaThesisTopic.Select(d => d.Degree).Distinct();
+            var availability = db.DiplomaThesisTopic.Select(d => d.Availability).Distinct();
+
+            ViewBag.NumberOfStudents = new SelectList(numberOfStudents);
+            ViewBag.FieldOfStudy = new SelectList(fieldOfStudy);
+            ViewBag.Degree = new SelectList(degree);
+            ViewBag.Availability = new SelectList(availability);
+
+            // think about better method
+            if(diplomaThesisTopic.NumberOfStudents != null)
+            {
+                topicList = topicList.Where(d => d.NumberOfStudents == diplomaThesisTopic.NumberOfStudents);
+            }
+            if(diplomaThesisTopic.FieldOfStudy != null)
+            {
+                topicList = topicList.Where(d => d.FieldOfStudy == diplomaThesisTopic.FieldOfStudy);
+            }
+            if(diplomaThesisTopic.Degree != null)
+            {
+                topicList = topicList.Where(d => d.Degree == diplomaThesisTopic.Degree);
+            }
+            if(diplomaThesisTopic.Availability != null)
+            {
+                topicList = topicList.Where(d => d.Availability == diplomaThesisTopic.Availability);
+            }
+
+            return View(topicList.ToList());
+        }
+
+
+    }
+}
